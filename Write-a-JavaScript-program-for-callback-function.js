ad3e2b43528a32c813ecb385383fcfb717391d36
Mohsin <ali.mohsin38@gmail.com>
2017-03-20 18:24:25 +0530

    Other methods for callback function Implemented...
    ES6 implemented...

--- Write-a-JavaScript-program-for-callback-function.js
+++ Write-a-JavaScript-program-for-callback-function.js
@@ -1,3 +1,15 @@
 (function(){
     setTimeout(function(){console.log("Welcome In The JavaScript World Environment !!")}, 1000);
 })();
+
+//OR
+
+var arr = [8, 6, 0, 55, 2, 67];
+function descendingSort(a, b){return b-a}
+arr.sort(descendingSort);      // CallBack Function
+
+//OR
+
+var arr = [8, 6, 0, 55, 2, 67];
+var descendingSort = (a, b)=>{return b-a}  // ECMA6 function declaration. Here "()=>{}" = "function(){}"
+arr.sort(descendingSort);      // CallBack Function
