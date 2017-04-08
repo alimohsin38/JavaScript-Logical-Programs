d2014dcd602178808ea6d04103e829bd2808e45d
Mohsin Ali <ali.mohsin38@gmail.com>
2017-04-09 00:16:06 +0530

    Updated wit setInterval function !
    Updated wit setInterval function !

--- Write-a-JavaScript-program-to-print-0-to-9-numbers-after-one-one-seconds.js
+++ Write-a-JavaScript-program-to-print-0-to-9-numbers-after-one-one-seconds.js
@@ -9,3 +9,14 @@ for(var i=0; i<10; i++){
 	(function (index){setTimeout(function(){console.log(index);}, 1000*i)})(i);
 }         //OutPut will be 0 1 2 3 4 5 6 7 8 9
 
+
+//Also we can do this with "setInterval" function. Below code is showing that how to be do it...
+var counter = 0;
+var idx = setInterval(function(){
+		console.log(counter);
+		counter++;
+		if(counter >= 10){
+			clearInterval(idx);
+		}
+	  },1000);     //OutPut will be 0 1 2 3 4 5 6 7 8 9
+
